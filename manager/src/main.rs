570175ebc64efdc5ba58b0cee1765a76502e9335
janmarcheckman <janmarcheckman@gmail.com>
2023-12-06 12:05:19 +0100

    small refactor

--- manager/src/main.rs
+++ manager/src/main.rs
@@ -2,11 +2,11 @@ use std::{sync::Arc, collections::HashMap, convert::Infallible};
 
 use console_input::console_input_thread;
 use context::{MucoContextRef, MucoContext};
-use msgs::{client_server_msg::ClientServerMsg, client_type::ClientType, server_client_msg::ServerClientMsg};
+use msgs::{client_server_msg::ClientServerMsg, client_type::ClientType};
 use process_server_client_msg::process_server_client_msg;
 use relay_server_connection_process::spawn_relay_server_connection_process;
 use status::Status;
-use tokio::sync::{mpsc::Receiver, RwLock};
+use tokio::sync::RwLock;
 use warp::{reject::Rejection, Filter};
 
 mod color;
@@ -32,7 +32,7 @@ const DEFAULT_SESSION_DURATION: i64 = 30 * 60;
 async fn main() {
     let status = Status::load(SAVE_DATA_PATH).unwrap_or(Status::new());
 
-    let (server_to_main, main_from_server) = tokio::sync::mpsc::channel(100);
+    let (server_to_main, mut main_from_server) = tokio::sync::mpsc::channel(100);
     let to_relay_server_process = spawn_relay_server_connection_process(server_to_main);
 
     to_relay_server_process.send(ClientServerMsg::SetClientType (ClientType::Manager)).await.unwrap();
@@ -63,10 +63,6 @@ async fn main() {
         warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
     });
 
-    relay_server(main_from_server, context_ref).await;
-}
-
-pub async fn relay_server(mut main_from_server: Receiver<ServerClientMsg>, context_ref: MucoContextRef) {
     loop {
         let Some(msg) = main_from_server.recv().await else { break };
         process_server_client_msg(msg, &context_ref).await;
