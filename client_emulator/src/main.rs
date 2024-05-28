5eb7c1d1cf8d27fa2c2ac4b2436f9918ee8bfa87
janmarcheckman <janmarcheckman@gmail.com>
2024-05-28 12:45:58 +0200

    basic setup client_emulator

--- client_emulator/src/main.rs
+++ client_emulator/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use msgs::relay_server_connection_process::spawn_relay_server_connection_process;
+
+#[tokio::main]
+async fn main() {
+    let (server_to_main, mut _main_from_server) = tokio::sync::mpsc::channel(100);
+    let _to_relay_server_process = spawn_relay_server_connection_process(server_to_main);
+    loop {
+        
+    }
 }
