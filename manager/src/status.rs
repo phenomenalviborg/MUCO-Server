bc94ab885fe594686bf631a3e4b22945e969faf5
janmarcheckman <janmarcheckman@gmail.com>
2024-10-09 13:10:42 +0200

    save environment codes

--- manager/src/status.rs
+++ manager/src/status.rs
@@ -11,6 +11,12 @@ pub struct Status {
     pub environment_codes: HashMap<EnvCodeName, Box<str>>,
 }
 
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct SaveData {
+    pub headsets: Vec<PersistentHeadsetData>,
+    pub environment_codes: HashMap<EnvCodeName, Box<str>>,
+}
+
 impl Status {
     pub fn new() -> Status {
         let mut environment_codes = HashMap::new();
@@ -23,21 +29,26 @@ impl Status {
 
     pub fn save(&self, path: &str) -> anyhow::Result<()>{
         let persistent_data = self.headsets.iter().map(|(_, headset_data)| headset_data.persistent.clone()).collect::<Vec<_>>();
-        let json = serde_json::to_string_pretty(&persistent_data)?;
+        let save_data = SaveData {
+            headsets: persistent_data,
+            environment_codes: self.environment_codes.clone(),
+        };
+        let json = serde_json::to_string_pretty(&save_data)?;
         std::fs::write(path, json)?;
         Ok(())
     }
 
     pub fn load(path: &str) -> anyhow::Result<Status> {
         let json = std::fs::read_to_string(path)?;
-        let persistent_data = serde_json::from_str::<Vec<PersistentHeadsetData>>(&json)?;
+        let save_data = serde_json::from_str::<SaveData>(&json)?;
         let mut status = Status::new();
-        for persistent in persistent_data {
+        for persistent in save_data.headsets {
             let k = persistent.unique_device_id;
             let temp = TempHeadsetData::new();
             let v = HeadsetData { persistent, temp };
             status.headsets.insert(k, v);
         }
+        status.environment_codes = save_data.environment_codes;
         Ok(status)
     }
 }
